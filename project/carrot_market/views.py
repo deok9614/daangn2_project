ac5219503553ad97d7cf953183dfd7a62cd0e229
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2023-09-21 17:48:33 +0900

    Add : db 수정, 미디어 경로 추가, view.write 추가

--- project/carrot_market/views.py
+++ project/carrot_market/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
-
+from .models import Post
+from .forms import PostForm
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 def login(request):
     pass
 def chat(request):
@@ -11,3 +15,21 @@ def trade(request):
     pass
 def trade_post(request):
     pass
+
+# @login_required
+def write(request, product_id=None):
+    # 업로드
+    if request.method == 'POST':
+        form = PostForm(request.POST) # 폼 초기화
+        if form.is_valid():
+            post = form.save(using='daangn2')
+            return redirect('carrot_market:trade_post', product_id=post.product_id)
+    # 폼을 초기화함
+    else:
+        form = PostForm()
+
+    context = {'form': form, 'MEDIA_URL': settings.MEDIA_URL,}
+    return render(request,'dangun_app/write.html' , context)
+
+def main(request):
+    return render(request, 'dangun_app/main.html')
