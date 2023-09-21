606599827fc9b4528716be6dee7d2b7d43c9ae39
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2023-09-21 14:45:07 +0900

    Add : 기본 뷰

--- project/carrot_market/views.py
+++ project/carrot_market/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def login(request):
+    pass
+def chat(request):
+    pass
+def location(request):
+    pass
+def trade(request):
+    pass
+def trade_post(request):
+    pass
