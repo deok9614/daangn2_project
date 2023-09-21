7ff79e3aecf9f4da1b7f6251aa1a52f1f3ca7f50
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2023-09-21 14:36:59 +0900

    Add : setting 데이터베이스

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "carrot_market",
+    "carrot_market",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "5432",
     }
 }
 
