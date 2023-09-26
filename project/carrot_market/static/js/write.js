c25b6f08961f4a3fb8cbb2c2e9d4ece993c05d65
juni9797 <wnsgml2882@naver.com>
2023-09-26 09:18:40 +0900

    feat : 브랜치 청소

--- project/carrot_market/static/js/write.js
+++
@@ -1,9 +0,0 @@
-function previewImage(event) {
-  let reader = new FileReader();
-  reader.onload = function () {
-    let output = document.getElementById("imagePreview");
-    output.src = reader.result;
-    output.classList.add("img-upload-fit");
-  };
-  reader.readAsDataURL(event.target.files[0]);
-}
