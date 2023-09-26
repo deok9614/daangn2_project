9849bd7fe69af5212f68e548759af2b6ef6f5f5b
juni9797 <wnsgml2882@naver.com>
2023-09-26 09:17:17 +0900

    feat : 브랜치 청소

--- project/carrot_market/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Post
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        exclude = ['created_at', 'views', 'user_id']
-        # 아이디 생성 시 user_id 제거
-
