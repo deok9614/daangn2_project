a790e0efb9820be4b163d53d79a118a866547353
juni9797 <wnsgml2882@naver.com>
2023-10-04 14:57:06 +0900

    add : chat

--- project/carrot_market/forms.py
+++ project/carrot_market/forms.py
@@ -8,3 +8,31 @@ class PostForm(forms.ModelForm):
         exclude = ['created_at', 'views', 'user_id','product_img', 'chat_num', 'product_sold', 'product_reserved']
         # 아이디 생성 시 user_id 제거
     # images = forms.FileField(widget=forms.ClearableFileInput(attrs={'allow_multiple_selected': True}))
+class CustomLoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '아이디를 입력해주세요', 'class': 'login-input'}),
+        label='아이디',
+        label_suffix='', 
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호',
+        label_suffix='', 
+    )
+
+class CustomRegistrationForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '아이디를 입력해주세요', 'class': 'login-input'}),
+        label='아이디',
+        label_suffix='', 
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호',
+        label_suffix='', 
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 다시 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호 확인',
+        label_suffix='', 
+    )
--- project/carrot_market/views.py
+++ project/carrot_market/views.py
@@ -8,6 +8,9 @@ from .forms import PostForm
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as auth_login
+from django.http import JsonResponse
+from django.db.models import Q
+from .forms import CustomLoginForm, CustomRegistrationForm, PostForm
 
 def login(request):
     if request.method == 'POST':
@@ -150,3 +153,134 @@ def main(request):
     
 #     context = {'form': form}
 #     return render(request, 'dangun_app/write.html', context)
+
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import CustomRegistrationForm
+
+def register(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = CustomRegistrationForm(request.POST)
+        username = request.POST.get('username')
+        if User.objects.filter(username=username).exists():
+            error_message = "이미 존재하는 아이디입니다."
+        elif form.is_valid():
+            
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            
+            # 비밀번호 일치 여부를 확인
+            if password1 == password2:
+                # 새로운 유저를 생성
+                user = User.objects.create_user(username=username, password=password1)
+                
+                # 유저를 로그인 상태로 만듦
+                login(request, user)
+            
+            
+                return redirect('dangun_app:login')
+            else:
+                form.add_error('password2', 'Passwords do not match')
+    else:
+        form = CustomRegistrationForm()
+    
+    return render(request, 'registration/register.html', {'form': form, 'error_message': error_message})
+
+
+def custom_login(request):
+    # 이미 로그인한 경우
+    if request.user.is_authenticated:
+        return redirect('dangun_app:main')
+    
+    else:
+        form = CustomLoginForm(data=request.POST or None)
+        if request.method == "POST":
+
+            # 입력정보가 유효한 경우 각 필드 정보 가져옴
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+
+                # 위 정보로 사용자 인증(authenticate사용하여 superuser로 로그인 가능)
+                user = authenticate(request, username=username, password=password)
+
+                # 로그인이 성공한 경우
+                if user is not None:
+                    login(request, user) # 로그인 처리 및 세션에 사용자 정보 저장
+                    return redirect('dangun_app:main')  # 리다이렉션
+        return render(request, 'registration/login.html', {'form': form}) #폼을 템플릿으로 전달
+    
+    
+def chat_room_test(request):
+    return render(request, 'dangun_app/chat_room.html')
+
+def chat_room(request, pk):
+    user = request.user
+    chat_room = get_object_or_404(ChatRoom, pk=pk)
+
+    # 내 ID가 포함된 방만 가져오기
+    chat_rooms = ChatRoom.objects.filter(
+            Q(receiver_id=user) | Q(starter_id=user)
+        ).order_by('-latest_message_time')  # 최신 메시지 시간을 기준으로 내림차순 정렬
+    
+    # 각 채팅방의 최신 메시지를 가져오기
+    chat_room_data = []
+    for room in chat_rooms:
+        latest_message = Message.objects.filter(chatroom=room).order_by('-timestamp').first()
+        if latest_message:
+            chat_room_data.append({
+                'chat_room': room,
+                'latest_message': latest_message.content,
+                'timestamp': latest_message.timestamp,
+            })
+
+    # 상대방 정보 가져오기
+    if chat_room.receiver == user:
+        opponent = chat_room.starter
+    else:
+        opponent = chat_room.receiver
+
+    opponent_user = User.objects.get(pk=opponent.pk)
+
+
+    # post의 상태 확인 및 처리
+    if chat_room.post is None:
+        seller = None
+        post = None
+    else:
+        seller = chat_room.post.user
+        post = chat_room.post
+
+    return render(request, 'dangun_app/chat_room.html', {
+        'chat_room': chat_room,
+        'chat_room_data': chat_room_data,
+        'room_name': chat_room.pk,
+        'seller': seller,
+        'post': post,
+        'opponent': opponent_user,
+    })
+
+
+# 채팅방 생성 또는 참여
+def create_or_join_chat(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    user = request.user
+    chat_room = None
+    created = False
+
+    # 채팅방이 이미 존재하는지 확인
+    chat_rooms = ChatRoom.objects.filter(
+        Q(starter=user, receiver=post.user, post=post) |
+        Q(starter=post.user, receiver=user, post=post)
+    )
+    if chat_rooms.exists():
+        chat_room = chat_rooms.first()
+    else:
+        # 채팅방이 존재하지 않는 경우, 새로운 채팅방 생성
+        chat_room = ChatRoom(starter=user, receiver=post.user, post=post)
+        chat_room.save()
+        created = True
+
+    return JsonResponse({'success': True, 'chat_room_id': chat_room.pk, 'created': created})
