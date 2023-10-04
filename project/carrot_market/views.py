90c4b8b78cc34e913d0d3b5a63b8a6621209f92f
juni9797 <wnsgml2882@naver.com>
2023-10-04 15:02:24 +0900

    feat : 채팅방 뷰 수정

--- project/carrot_market/views.py
+++ project/carrot_market/views.py
@@ -158,59 +158,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from .forms import CustomRegistrationForm
-
-def register(request):
-    error_message = ''
-    if request.method == 'POST':
-        form = CustomRegistrationForm(request.POST)
-        username = request.POST.get('username')
-        if User.objects.filter(username=username).exists():
-            error_message = "이미 존재하는 아이디입니다."
-        elif form.is_valid():
-            
-            password1 = form.cleaned_data['password1']
-            password2 = form.cleaned_data['password2']
-            
-            # 비밀번호 일치 여부를 확인
-            if password1 == password2:
-                # 새로운 유저를 생성
-                user = User.objects.create_user(username=username, password=password1)
-                
-                # 유저를 로그인 상태로 만듦
-                login(request, user)
-            
-            
-                return redirect('dangun_app:login')
-            else:
-                form.add_error('password2', 'Passwords do not match')
-    else:
-        form = CustomRegistrationForm()
-    
-    return render(request, 'registration/register.html', {'form': form, 'error_message': error_message})
-
-
-def custom_login(request):
-    # 이미 로그인한 경우
-    if request.user.is_authenticated:
-        return redirect('dangun_app:main')
-    
-    else:
-        form = CustomLoginForm(data=request.POST or None)
-        if request.method == "POST":
-
-            # 입력정보가 유효한 경우 각 필드 정보 가져옴
-            if form.is_valid():
-                username = form.cleaned_data['username']
-                password = form.cleaned_data['password']
-
-                # 위 정보로 사용자 인증(authenticate사용하여 superuser로 로그인 가능)
-                user = authenticate(request, username=username, password=password)
-
-                # 로그인이 성공한 경우
-                if user is not None:
-                    login(request, user) # 로그인 처리 및 세션에 사용자 정보 저장
-                    return redirect('dangun_app:main')  # 리다이렉션
-        return render(request, 'registration/login.html', {'form': form}) #폼을 템플릿으로 전달
     
     
 def chat_room_test(request):
