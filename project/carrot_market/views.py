bc2f1a7531090f0097bf13098ad5841b17990b0b
dlwnsgur9242 <dlwnsgur9242@naver.com>
2023-10-05 11:33:25 +0900

    views.py+nav.html 수정

--- project/carrot_market/views.py
+++ project/carrot_market/views.py
@@ -6,26 +6,66 @@ from .models import Post, User
 from .forms import PostForm
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Post, UserProfile, PostImage
+from .forms import PostForm, CustomLoginForm, CustomUserForm
+from django.contrib import messages
+from social_core.backends.google import GoogleOAuth2
+from social_core.backends.naver import NaverOAuth2
 
-def login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)  # AuthenticationForm 객체 생성
-        if form.is_valid():  # 폼 유효성 검사
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:  # 사용자가 인증되었을 때만
-                login(request, user)  # 로그인 함수에 사용자 객체를 전달합니다.
-                return HttpResponse("로그인 성공!")
-            else:
-                return HttpResponse("로그인 실패!")
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('dangun_app:main')
     else:
-        form = AuthenticationForm()  # GET 요청 시 폼 생성
-    return render(request, 'registration/login.html', {"form": form})  # 폼을 템플릿에 전달
+        form = CustomLoginForm(data=request.POST or None)
+        if request.method == 'POST':
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                # 사용자가 선택한 로그인 백엔드를 가져옵니다.
+                backend = request.POST.get('login_with')
+
+                # 선택한 백엔드에 따라 사용자를 인증합니다.
+                if backend == 'google':
+                    user = authenticate(request, backend=GoogleOAuth2.name)
+                elif backend == 'naver':
+                    user = authenticate(request, backend=NaverOAuth2.name)
+                else:
+                    user = authenticate(request, username=username, password=password)
+
+                if user is not None:
+                    # 수정 중!!
+                    login(request, user)
+                    return redirect('dangun_app:main')
+                else:
+                    return HttpResponse("로그인 실패!")
+        return render(request, 'registration/login.html', {'form': form})
 
 def register(request):
-    return render(request,'registration/register.html')
+    error_message = ''
+    if request.method == 'POST':
+        form = CustomUserForm(request.POST)
+        username = request.POST.get('username')
+        if User.objects.filter(username=username).exists():
+            error_message = "이미 존재하는 아이디입니다."
+        elif form.is_valid():
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
 
+            if password1 == password2:
+                user = User.objects.create_user(username=username, password=password1)
+                login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+                return redirect('dangun_app:main')
+            else:
+                form.add_error('password2', '비밀번호가 일치하지 않습니다')
+    else:
+        form = CustomUserForm()
+
+    return render(request, 'registration/register.html', {'form': form, 'error_message': error_message})
 def chat(request):
     return render(request, 'dangun_app/chat.html')
 
